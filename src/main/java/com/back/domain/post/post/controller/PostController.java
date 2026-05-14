84fba08c09be48eb15f75b5e317fc3178fb88212
JUNHYEONG LEE <nibble1492@gmail.com>
2026-05-14 15:02:08 +0900

    refactor: PostController 패키지 이동 및 게시글 작성 유효성 검사 추가

---
+++ src/main/java/com/back/domain/post/post/controller/PostController.java
@@ -0,0 +1,56 @@
+package com.back.domain.post.post.controller;
+
+import com.back.domain.post.post.entity.Post;
+import com.back.domain.post.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class PostController {
+    private final PostService postService;
+
+    private String getWriteFormHtml() {
+        return getWriteFormHtml("");
+    }
+
+    private String getWriteFormHtml(String errorMessage) {
+        return """
+                <div style="color: red;">%s</div>
+                
+                <form method="POST" action="doWrite">
+                  <input type="text" name="title" placeholder="제목" value="">
+                  <br>
+                  <textarea name="content" placeholder="내용"></textarea>
+                  <br>
+                  <input type="submit" value="작성">
+                </form>
+                """.formatted(errorMessage);
+    }
+
+    @GetMapping("/posts/write")
+    @ResponseBody
+    public String write() {
+        return getWriteFormHtml();
+    }
+
+    @PostMapping("/posts/doWrite")
+    @ResponseBody
+    @Transactional
+    public String doWrite(
+            @RequestParam(defaultValue = "") String title,
+            @RequestParam(defaultValue = "") String content
+    ) {
+        if (title.isBlank()) return getWriteFormHtml("제목을 입력해주세요.");
+        if (content.isBlank()) return getWriteFormHtml("내용을 입력해주세요.");
+
+        Post post = postService.write(title, content);
+
+        return "%d번 글이 생성되었습니다.".formatted(post.getId());
+    }
+}
