a8187b776a86b0bcbf0d41e59535a69a6094223c
JUNHYEONG LEE <nibble1492@gmail.com>
2026-05-15 09:56:01 +0900

    refactor: WriteForm DTO를 이용한 게시글 작성 유효성 검사 방식으로 변경

--- src/main/java/com/back/domain/post/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/post/controller/PostController.java
@@ -2,6 +2,9 @@ package com.back.domain.post.post.controller;
 
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,7 +18,6 @@ import jakarta.validation.constraints.Size;
 
 @Controller
 @RequiredArgsConstructor
-@Validated
 public class PostController {
     private final PostService postService;
 
@@ -62,20 +64,24 @@ public class PostController {
         return getWriteFormHtml();
     }
 
+    @AllArgsConstructor
+    @Getter
+    public static class WriteForm {
+        @NotBlank
+        @Size(min = 2, max = 20)
+        private String title;
+        @NotBlank
+        @Size(min = 2, max = 20)
+        private String content;
+    }
+
     @PostMapping("/posts/doWrite")
     @ResponseBody
     @Transactional
     public String doWrite(
-            @NotBlank
-            @Size(min = 2, max = 20)
-            @RequestParam(defaultValue = "")
-            String title,
-            @NotBlank
-            @Size(min = 2, max = 100)
-            @RequestParam(defaultValue = "")
-            String content
+            @Valid WriteForm form
     ) {
-        Post post = postService.write(title, content);
+        Post post = postService.write(form.getTitle(), form.getContent());
 
         return "%d번 글이 생성되었습니다.".formatted(post.getId());
     }
