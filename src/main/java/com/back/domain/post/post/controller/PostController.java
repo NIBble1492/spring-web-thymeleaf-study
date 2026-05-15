860e9930880159efbbe07d7e29377a9fc531eb97
JUNHYEONG LEE <nibble1492@gmail.com>
2026-05-15 13:17:05 +0900

    refactor: 타임리프 #fields를 활용한 에러 메시지 렌더링 및 컨트롤러 로직 간소화

--- src/main/java/com/back/domain/post/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/post/controller/PostController.java
@@ -51,20 +51,8 @@ public class PostController {
             BindingResult bindingResult,
             Model model
     ) {
-        if (bindingResult.hasErrors()) {
+        if (bindingResult.hasErrors()) return "post/post/write";
 
-            String errorMessage = bindingResult
-                    .getFieldErrors()
-                    .stream()
-                    .map(fieldError -> (fieldError.getField() + "-" + fieldError.getDefaultMessage()).split("-", 3))
-                    .map(field -> "<!--%s--><li data-error-field-name=\"%s\">%s</li>".formatted(field[1], field[0], field[2]))
-                    .sorted()
-                    .collect(Collectors.joining("\n"));
-
-            model.addAttribute("errorMessage", errorMessage);
-
-            return "post/post/write";
-        }
         Post post = postService.write(form.getTitle(), form.getContent());
 
         model.addAttribute("post", post);
